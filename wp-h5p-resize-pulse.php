8ed85c69812bd2e918fc8cb9cc6a0a5735c961f7
Oliver Tacke <o.tacke@posteo.de>
2023-07-23 13:29:07 +0200

    Fix header line break after php intro not allowed

--- wp-h5p-resize-pulse.php
+++ wp-h5p-resize-pulse.php
@@ -1,15 +1,15 @@
 <?php
-
 /**
  * Plugin Name: SNORDIAN's H5P Resize Pulse
  * Plugin URI: https://github.com/otacke/wp-h5p-resize-pulse
- * Text Domain: H5PRESIZEPULSE
- * Domain Path: /languages
  * Description: Provides you with a potential workaround for H5P content that won't show in tabs, accordions, lightboxes, etc.
  * Version: 0.1.2
  * Author: Oliver Tacke
  * Author URI: https://www.olivertacke.de/labs
  * License: MIT
+ * License URI:
+ * Text Domain: H5PRESIZEPULSE
+ * Domain Path: /languages
  */
 
 namespace H5PRESIZEPULSE;
