ccb2744ad27e138679fd2dc7e1f4b76f02d539e9
Oliver Tacke <o.tacke@posteo.de>
2023-09-29 13:04:37 +0200

    Add resizeObserver option

--- js/h5p-resize-pulse.js
+++ js/h5p-resize-pulse.js
@@ -1,64 +1,108 @@
-( function() {
+(() => {
+  let resizeMechanismStarted = false;
 
-  var h5pResizePulseStarted = false;
-
-  // h5pResizePulseParameters {object} should be passed by WordPress
-  h5pResizePulseParameters = h5pResizePulseParameters || {};
+  // userParams {object} should be passed by WordPress
+  const userParams = window.h5pResizePulseParameters || {};
 
   // Simple sanitizing
-  h5pResizePulseParameters.timeout =
-    parseInt( h5pResizePulseParameters.timeout );
+  userParams.timeout = parseInt(userParams.timeout);
 
   if (
-    isNaN( h5pResizePulseParameters.timeout ||
-    500 > h5pResizePulseParameters.timeout
-  ) ) {
-    h5pResizePulseParameters.timeout = 500;
+    isNaN(userParams.timeout) ||
+    userParams.timeout < 500
+  ) {
+    userParams.timeout = 500;
   }
 
   /**
+   * Trigger a resize event on the window that H5P core will recognize.
+   */
+  const triggerResize = () => {
+    console.log('DISPATCH');
+
+    window.dispatchEvent(new Event('resize'));
+  };
+
+  /**
    * Regularly trigger H5P resizing.
-   *
    * @param {number} timeout Timeout.
    */
-  function h5pResizePulse( timeout ) {
-    setTimeout( function() {
-      try {
-        window.dispatchEvent( new Event( 'resize' ) );
-      } catch ( error ) {
-        window.dispatchEvent( new window.CustomEvent( 'resize' ) );
-      }
+  const scheduleResizePulse = (timeout) => {
+    setTimeout(() => {
+      triggerResize();
+      scheduleResizePulse(timeout);
+    }, timeout);
+  };
 
-      h5pResizePulse( timeout );
-    }, timeout );
+  /**
+   * Add resize pulse.
+   * @returns {boolean} True, if resize mechanism could be started, else false.
+   */
+  const addResizePulse = () => {
+    if (!window.H5P || !window.H5P.externalDispatcher) {
+      return false; // H5P not present, but should be by now
+    }
+
+    // Resize once an H5P instance is initialized and resize might be needed
+    window.H5P.externalDispatcher.once('initialized', () => {
+      scheduleResizePulse(userParams.timeout);
+    });
+
+    return true;
   };
 
   /**
    * Add resize triggers.
    *
    * @param {string} selector CSS selector for elements.
+   * @returns {boolean} True, if resize mechanism could be started, else false.
    */
-  function addH5pResizeTriggers( selector ) {
-    var index;
-    var triggerElements;
-
-    try {
-      triggerElements = document.querySelectorAll( selector );
-    } catch ( error ) {
-      triggerElements = null;
+  const addClickListeners = (selector) => {
+    if (typeof userParams.selector !== 'string') {
+      return false; // No valid selector
     }
 
-    if ( ! triggerElements ) {
+    let triggerElements;
+    try {
+      triggerElements = Array.from(document.querySelectorAll(selector));
+    } catch (error) {
+      triggerElements = [];
+    }
+
+    if (!triggerElements.length) {
       return false; // No elements found
     }
 
-    for ( index = 0; index < triggerElements.length; index++ ) {
-      triggerElements[index].addEventListener( 'click', function() {
-        window.requestAnimationFrame( function() {
-          window.dispatchEvent( new window.CustomEvent( 'resize' ) );
+    triggerElements.forEach((element) => {
+      element.addEventListener('click', () => {
+        // DOM will have to have re-rendered, so height can be determined
+        window.requestAnimationFrame(() => {
+          triggerResize();
         });
       });
+    });
+
+    return true;
+  };
+
+  /**
+   * Add resize observer to each H5P iframe parent node.
+   * @returns {boolean} True, if resize mechanism could be started, else false.
+   */
+  const addResizeObservers = () => {
+    const h5pIframes = Array.from(
+      document.body.querySelectorAll('iframe.h5p-iframe')
+    );
+
+    if (!h5pIframes.length) {
+      return false;
     }
+
+    h5pIframes.forEach((iframe) => {
+      (new ResizeObserver(() => {
+        triggerResize();
+      })).observe(iframe.parentNode);
+    });
 
     return true;
   };
@@ -66,37 +110,24 @@
   /**
    * Try to start.
    */
-  function tryToStart() {
-    if (
-      'selector' === h5pResizePulseParameters.mode &&
-      'string' === typeof h5pResizePulseParameters.selector
-    ) {
-      h5pResizePulseStarted =
-        addH5pResizeTriggers( h5pResizePulseParameters.selector );
-    } else {
-      if ( ! window.H5P || ! window.H5P.externalDispatcher ) {
-        return; // H5P not present, but should by now
-      }
-
-      h5pResizePulseStarted = true;
-
-      // Resize once an H5P instance is initialized and resize might be needed
-      window.H5P.externalDispatcher.once( 'initialized', function() {
-        h5pResizePulse( h5pResizePulseParameters.timeout );
-      });
+  const tryToStart = () => {
+    if (userParams.mode === 'observer') {
+      resizeMechanismStarted = addResizeObservers();
+    } else if (userParams.mode === 'selector') {
+      resizeMechanismStarted = addClickListeners(userParams.selector);
+    } else if (userParams.mode === 'interval') {
+      resizeMechanismStarted = addResizePulse();
     }
   };
 
-  document.addEventListener( 'readystatechange', function() {
-    if ( 'interactive' === document.readyState ) {
+  document.addEventListener('readystatechange', () => {
+    if ('interactive' === document.readyState) {
       tryToStart();
-    } else if ( 'complete' === document.readyState ) {
-
+    } else if ('complete' === document.readyState) {
       // Plugin might not have rendered elements on 'interactive'
-      if ( ! h5pResizePulseStarted ) {
+      if (!resizeMechanismStarted) {
         tryToStart();
       }
     }
   });
-
-}() );
+})();
